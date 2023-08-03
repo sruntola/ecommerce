f61c47bc6e679892f421ccd29396cffa2d9a9151
sruntola <sruntola@Sruns-MacBook-Pro-2.local>
2023-08-03 21:27:26 +0700

    update

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ require("./app/routes/payment.routes")(app);
 require("./app/routes/size.routes")(app);
 require("./app/routes/variants.routes")(app);
 require("./app/routes/gender.routes")(app);
-db.sequelize.sync({force: true});
+db.sequelize.sync();
 
 app.listen(port, () => {
   console.log(`App port ${port}.`);
